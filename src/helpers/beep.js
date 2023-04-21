11ef5081b5605c8a268c5cc51188f68dccb66972
Nathan Long <nlong5@gmail.com>
2023-04-21 11:48:14 -0400

    feat(audio): change to single tone with convolver reverb, and slightly bigger buttons

--- src/helpers/beep.js
+++ src/helpers/beep.js
@@ -1,56 +1,57 @@
 function beep(audioCX) {
-  // Cmaj9/E = E3 161.82, C4 256.87, D4 288.33, G4 384.87, B4 484.90
   // Cmaj9/E+1 = E4 323.63, C5 513.74, D5 576.65, G5 769.74, B5 969.81
   // https://pages.mtu.edu/~suits/notefreq432.html
-  const tone1 = audioCX.current.createOscillator()
-  const tone2 = audioCX.current.createOscillator()
-  const tone3 = audioCX.current.createOscillator()
-  const tone4 = audioCX.current.createOscillator()
-  const tone5 = audioCX.current.createOscillator()
-  tone1.type = 'sine'
-  tone2.type = 'sine'
-  tone3.type = 'sine'
-  tone4.type = 'sine'
-  tone5.type = 'sine'
-  tone1.frequency.setValueAtTime(323.63, audioCX.current.currentTime)
-  tone2.frequency.setValueAtTime(513.74, audioCX.current.currentTime)
-  tone3.frequency.setValueAtTime(576.65, audioCX.current.currentTime)
-  tone4.frequency.setValueAtTime(769.74, audioCX.current.currentTime)
-  tone5.frequency.setValueAtTime(969.81, audioCX.current.currentTime)
+  const chord = ["323.63", "513.74", "576.65", "769.74", "969.81"];
+  const randomTone = Math.floor(Math.random() * chord.length);
 
-  // keep it from getting muddy
-  const compressor = audioCX.current.createDynamicsCompressor();
-  compressor.threshold.setValueAtTime(-70, audioCX.current.currentTime);
-  compressor.knee.setValueAtTime(10, audioCX.current.currentTime);
-  compressor.ratio.setValueAtTime(20, audioCX.current.currentTime);
-  compressor.attack.setValueAtTime(0, audioCX.current.currentTime);
-  compressor.release.setValueAtTime(0.25, audioCX.current.currentTime)
+  // build oscillator
+  const tone1 = audioCX.current.createOscillator();
+  tone1.type = "sine";
+  tone1.frequency.setValueAtTime(
+    chord[randomTone],
+    audioCX.current.currentTime
+  );
+
+  //convolver reverb
+  const convolver = audioCX.current.createConvolver();
+
+  // Grab audio track via XHR for convolver node
+  let soundSource;
+  let ajaxRequest = new XMLHttpRequest();
+
+  ajaxRequest.open("GET", "/audio/waves.mp3", true);
+  ajaxRequest.responseType = "arraybuffer";
+  ajaxRequest.onload = function () {
+    const audioData = ajaxRequest.response;
+    audioCX.current.decodeAudioData(
+      audioData,
+      function (buffer) {
+        soundSource = audioCX.current.createBufferSource();
+        convolver.buffer = buffer;
+      },
+      function (e) {
+        console.log("Error with decoding audio data" + e.err);
+      }
+    );
+  };
+  ajaxRequest.send();
 
   // fade out
-  const jumpGain = audioCX.current.createGain()
-  jumpGain.gain.setValueAtTime(20, audioCX.current.currentTime)
-  jumpGain.gain.exponentialRampToValueAtTime(0.001, audioCX.current.currentTime + 4)
+  const jumpGain = audioCX.current.createGain();
+  jumpGain.gain.setValueAtTime(1.5, audioCX.current.currentTime);
+  jumpGain.gain.exponentialRampToValueAtTime(
+    0.001,
+    audioCX.current.currentTime + 5
+  );
 
   // connect it all
-  tone1.connect(compressor)
-  tone2.connect(compressor)
-  tone3.connect(compressor)
-  tone4.connect(compressor)
-  tone5.connect(compressor)
-  compressor.connect(jumpGain)
-  jumpGain.connect(audioCX.current.destination)
+  tone1.connect(convolver);
+  convolver.connect(jumpGain);
+  jumpGain.connect(audioCX.current.destination);
 
-  // stagger chord
-  tone1.start(audioCX.current.currentTime)
-  tone1.stop(audioCX.current.currentTime + 4)
-  tone2.start(audioCX.current.currentTime + 0.10)
-  tone2.stop(audioCX.current.currentTime + 4)
-  tone3.start(audioCX.current.currentTime + 0.20)
-  tone3.stop(audioCX.current.currentTime + 4)
-  tone4.start(audioCX.current.currentTime + 0.30)
-  tone4.stop(audioCX.current.currentTime + 4)
-  tone5.start(audioCX.current.currentTime + 0.40)
-  tone5.stop(audioCX.current.currentTime + 4)
+  // play
+  tone1.start(audioCX.current.currentTime);
+  tone1.stop(audioCX.current.currentTime + 5);
 }
 
-export default beep
+export default beep;
